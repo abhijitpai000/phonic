9de29b0a1f5958a71fa0b654ba2d45e4b50ab2a3
Abhijit <abhijitpai000@gmail.com>
2021-04-18 04:51:52 +0530

    Update Data Models' Params

--- phonic/audio.py
+++ phonic/audio.py
@@ -4,11 +4,13 @@ Audio Blueprint.
 """
 
 # Flask Imports.
-from flask import Blueprint
+from flask import Blueprint, request
 
 # Local Imports.
 from phonic import create_app
 from phonic.models import init_db
+from phonic.models import Song, Podcast, AudioBook
+from phonic.models import SongSchema, PodcastSchema, AudioBookSchema
 
 # Init Audio Blueprint.
 audio_bp = Blueprint(name="audio",
@@ -20,7 +22,7 @@ app = create_app()
 db = init_db(app)
 
 
-@audio_bp.route("/audio/<audioFileType>/<audioFileID>", methods=["GET"])
+@audio_bp.route("/audio/<audioFileType>/<audioFileID>", methods=["POST"])
 def post_audio(audioFileType, audioFileID):
     """
     POST Audio.
@@ -32,5 +34,64 @@ def post_audio(audioFileType, audioFileID):
     audioFileID: int
         Audio File ID
 
-   """
-    return "Hello!"
+    Returns
+    -------
+    response: json.
+        Row Added to the Database.
+
+    """
+
+    if audioFileType == "song":
+        id = audioFileID
+        name = request.json["name"]
+        duration = request.json["duration"]
+
+        new_song = Song(id=id, name=name, duration=duration)
+
+        # Add new song to db.
+        db.session.add(new_song)
+        db.session.commit()
+
+        # Marshmallow Parser.
+        new_song_schema = SongSchema()
+        response = new_song_schema.jsonify(new_song)
+
+    elif audioFileType == "podcast":
+        id = audioFileID
+        name = request.json["name"]
+        duration = request.json["duration"]
+
+        new_podcast = Podcast(id=id,
+                              name=name,
+                              duration=duration)
+
+        # Add new podcast to db.
+        db.session.add(new_podcast)
+        db.session.commit()
+
+        # Marshmallow Parser.
+        new_podcast_schema = PodcastSchema()
+        response = new_podcast_schema.jsonify(new_podcast)
+
+    elif audioFileType == "audiobook":
+        id = audioFileID
+        title = request.json["title"]
+        author = request.json["author"]
+        narrator = request.json["narrator"]
+        duration = request.json["duration"]
+
+        new_audiobook = AudioBook(id=id,
+                                  name=title,
+                                  author=author,
+                                  narrator=narrator,
+                                  duration=duration)
+
+        # Add new podcast to db.
+        db.session.add(new_audiobook)
+        db.session.commit()
+
+        # Marshmallow Parser.
+        new_audiobook_schema = PodcastSchema()
+        response = new_audiobook_schema.jsonify(new_audiobook)
+
+    return response
