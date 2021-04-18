2e4772e206d21619e3535c6664f62050123d2e74
Abhijit <abhijitpai000@gmail.com>
2021-04-18 17:46:33 +0530

    Add init_db()

--- phonic/database.py
+++ phonic/database.py
@@ -5,3 +5,26 @@ Module with Initial Database Instance.
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+
+def init_db(app):
+    """
+    Create Databases using Schemas Defined.
+
+    Parameters
+    ----------
+    app: object.
+        Flask App Instance.
+
+    Returns
+    -------
+    db: object.
+        Database Instance.
+    """
+
+    # Create Tables.
+    with app.app_context():
+        db.create_all()
+        db.session.commit()
+
+    return db
